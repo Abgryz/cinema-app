96413fefedd060cb32349bb21edcc872d8f7775d
Abgryz <v.anufriev2003@gmail.com>
2023-05-15 18:19:47 +0300

    Revert "Added entities, DTOs, services."
    This reverts commit 1e1d3125cdb38a853dc7dd9cb63a7d7f4d3ca447.

--- src/main/java/com/suitt/tables/ticketSales/TicketSalesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.suitt.tables.ticketSales;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketSalesRepository extends JpaRepository<TicketSales, TicketSalesPK> {
-}
