68f92a3200b5c5aa21a45a9e24d2699673619403
Abgryz <v.anufriev2003@gmail.com>
2023-05-15 18:17:35 +0300

    Security

---
+++ src/main/java/com/suitt/Role.java
@@ -0,0 +1,5 @@
+package com.suitt;
+
+public enum Role {
+    USER, MANAGER, CASHIER;
+}
--- src/main/java/com/suitt/tables/client/Client.java
+++ src/main/java/com/suitt/tables/client/Client.java
@@ -1,21 +1,28 @@
 package com.suitt.tables.client;
 
 
+import com.suitt.Role;
 import com.suitt.tables.ticketSales.TicketSales;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "client")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Client {
+public class Client implements UserDetails {
     @Id
     @Column(name = "cl_phone_number")
     private String phoneNumber;
@@ -27,6 +34,47 @@ public class Client {
 
     private LocalDate birthDate;
 
+    private String password;
+
+    private boolean active;
+
+    private LocalDateTime dateOfCreation;
+
     @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<TicketSales> ticketSales;
+
+    @PrePersist
+    private void init(){
+        dateOfCreation = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
 }
